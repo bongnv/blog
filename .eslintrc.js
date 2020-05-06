6882e1156e0f7ba9292992008be81899c3dd2278
Bong Nguyen <van.bong@grabtaxi.com>
2020-05-06 22:09:03 +0800

    ci: add linters

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    "browser": true,
+    "es6": true,
+    "node": true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  parserOptions: {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  plugins: [
+    "react",
+    "prettier",
+    "react-hooks",
+  ],
+  "rules": {
+  }
+};
