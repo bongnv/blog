ca0ddc5f2980782318fd369217e3f6d100eb850c
Bong Nguyen <van.bong@grabtaxi.com>
2020-05-07 11:56:31 +0800

    fix: improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
+import Layout from "@/components/layout";
+import SEO from "@/components/seo";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <article className="w-full max-w-2xl content">
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </article>
   </Layout>
 );
 
