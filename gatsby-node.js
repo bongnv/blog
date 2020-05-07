14a9bb54292991daafd80d52f8f6d95f06cb2400
Bong Nguyen <van.bong@grabtaxi.com>
2020-05-07 10:21:53 +0800

    feat: add single blogpost

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,50 @@
 const path = require("path");
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve("src/templates/blog-post.js");
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        filter: { fields: { isBlog: { eq: true } } }
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  const edges = result.data.allMarkdownRemark.edges;
+  edges.forEach((edge, index) => {
+    const previous = index === edges.length - 1 ? null : edges[index + 1].node;
+    const next = index === 0 ? null : edges[index - 1].node;
+    createPage({
+      path: edge.node.fields.slug,
+      component: blogPostTemplate,
+      context: {
+        id: edge.node.id,
+        previous,
+        next,
+      },
+    });
+  });
+};
+
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
---
+++ src/templates/blog-post.js
@@ -0,0 +1,83 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+
+import Layout from "@/components/layout";
+import { Metadata } from "@/types/site-metadata";
+import PostMeta from "@/components/post-meta";
+
+const BlogPost = ({ data }) => {
+  const post = data.markdownRemark;
+  const headings = [];
+  const showSidebar = headings.length > 0;
+  return (
+    <Layout siteMetadata={data.site.siteMetadata}>
+      {showSidebar && <div className="hidden xl:block w-64" />}
+      <article className="w-full max-w-2xl">
+        <h1 className="text-center text-4xl text-foreground font-extrabold font-display mt-2x mb-1x">
+          {post.frontmatter.title}
+        </h1>
+        <div className="text-center mb-2x">
+          <PostMeta post={post} />
+        </div>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+      </article>
+      {showSidebar && (
+        <div className="hidden xl:block w-64">
+          {/* <Sidebar headings={headings} /> */}
+        </div>
+      )}
+    </Layout>
+  );
+};
+
+BlogPost.propTypes = {
+  data: PropTypes.shape({
+    site: PropTypes.shape({
+      siteMetadata: Metadata.isRequired,
+    }).isRequired,
+    markdownRemark: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      html: PropTypes.string.isRequired,
+      frontmatter: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      }),
+    }).isRequired,
+  }).isRequired,
+};
+
+export default BlogPost;
+
+export const pageQuery = graphql`
+  query BlogPostByID($id: String!) {
+    site {
+      siteMetadata {
+        author
+        title
+        links {
+          linkedin
+          github
+          source
+        }
+      }
+    }
+    markdownRemark(id: { eq: $id }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        description
+      }
+      fields {
+        readingTime {
+          minutes
+        }
+      }
+    }
+  }
+`;
