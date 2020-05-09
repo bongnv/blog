25835d527106b72d3e44d68b5d509603ce39aad4
Bong Nguyen <van.bong@grabtaxi.com>
2020-05-09 16:03:42 +0800

    fix: flickering when starting page

--- src/context/theme-context.tsx
+++ src/context/theme-context.tsx
@@ -2,24 +2,6 @@ import React from "react";
 
 const LIGHTS_OUT = "lights-out";
 
-const getInitialDarkMode = (): boolean => {
-  const darkMode = window.localStorage.getItem(LIGHTS_OUT);
-  if (darkMode === "true") {
-    return true;
-  }
-
-  if (darkMode === "false") {
-    return false;
-  }
-
-  const mql = window.matchMedia("(prefers-color-scheme: dark)");
-  if (typeof mql.matches === "boolean") {
-    return mql.matches;
-  }
-
-  return false;
-};
-
 const defaultContext = {
   darkMode: false,
   // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -35,7 +17,7 @@ interface ThemeProviderProps {
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({
   children,
 }: ThemeProviderProps) => {
-  const [darkMode, setDarkModeState] = React.useState(getInitialDarkMode());
+  const [darkMode, setDarkModeState] = React.useState(false);
   document.documentElement.toggleAttribute(LIGHTS_OUT, darkMode);
 
   const setDarkMode = (mode: boolean): void => {
