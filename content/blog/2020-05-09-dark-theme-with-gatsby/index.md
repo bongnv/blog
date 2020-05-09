5c70e542d714fb60d9b716b43a3282869b9797c7
Bong Nguyen <van.bong@grabtaxi.com>
2020-05-09 17:12:57 +0800

    fix: build with client codes

---
+++ content/blog/2020-05-09-dark-theme-with-gatsby/index.md
@@ -0,0 +1,6 @@
+---
+title: Enable dark theme in Gatsby
+date: 2020-05-09
+description: Dark theme is a cool feature but also a tough problem for a website. This post introduces a simple but effective way to enable dark theme in a Gatsby site.
+tags: ["react", "gatsby", "dark-theme"]
+---
--- src/components/dark-mode-switcher.tsx
+++ src/components/dark-mode-switcher.tsx
@@ -3,17 +3,19 @@ import { Moon, Sun } from "react-feather";
 
 const LIGHTS_OUT = "lights-out";
 
-const initialDarkMode = (): boolean =>
-  window.localStorage.getItem(LIGHTS_OUT) === "true";
-
 const DarkModeSwitcher: FC = () => {
-  const [darkMode, setDarkMode] = React.useState(initialDarkMode());
+  const [darkMode, setDarkMode] = React.useState(false);
   const handleClick = (): void => {
     const newMode = !darkMode;
     setDarkMode(newMode);
     document.documentElement.toggleAttribute(LIGHTS_OUT, newMode);
     window.localStorage.setItem(LIGHTS_OUT, newMode ? "true" : "false");
   };
+
+  React.useEffect(() => {
+    const isDarkMode = document.documentElement.hasAttribute(LIGHTS_OUT);
+    setDarkMode(isDarkMode);
+  }, []);
 
   return <button onClick={handleClick}>{darkMode ? <Moon /> : <Sun />}</button>;
 };
