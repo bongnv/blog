19f5f179399ff113679d28acea5de25d55d40a6c
Bong Nguyen <van.bong@grabtaxi.com>
2020-05-14 12:13:13 +0800

    feat: use head

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,20 +9,6 @@ import React from "react";
 import Layout from "./src/components/layout";
 import PropTypes from "prop-types";
 
-export const onRenderBody = ({ setHeadComponents }) => {
-  const script = `
-  const LIGHTS_OUT = "lights-out";
-  const darkMode = window.localStorage.getItem(LIGHTS_OUT) === "true";
-  document.documentElement.toggleAttribute(LIGHTS_OUT, darkMode);
-  `;
-  return setHeadComponents([
-    <script
-      key={`dark-mode-script`}
-      dangerouslySetInnerHTML={{ __html: script }}
-    />,
-  ]);
-};
-
 export const wrapPageElement = ({ element }) => {
   return <Layout>{element}</Layout>;
 };
--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -86,6 +86,11 @@ const SEO: FC<SEOProps> = ({
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap"
         rel="stylesheet"
       />
+      <script type="text/javascript">
+        {`const LIGHTS_OUT = "lights-out"; const darkMode =
+        window.localStorage.getItem(LIGHTS_OUT) === "true";
+        document.documentElement.toggleAttribute(LIGHTS_OUT, darkMode);`}
+      </script>
     </Helmet>
   );
 };
